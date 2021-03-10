04ae170b2adc96d3fabd54908742286ea152b9f9
RogerLianQing <68877438+RogerLianQing@users.noreply.github.com>
2021-03-10 11:34:38 -0500

    Update functions.php

--- twentyseventeen/functions.php
+++ twentyseventeen/functions.php
@@ -779,3 +779,30 @@ function add_a_button( $content ) {
 
 }
 }// t heeoooi
+
+// Video gif shortcode
+function videogif($atts = [])
+{
+    // normalize attribute keys, lowercase
+    $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+    // override default attributes with user attributes
+    $wporg_atts = shortcode_atts([
+            'mp4' => $atts['mp4'],
+            'style' => null,
+            'controls' => False
+        ], $atts);
+
+    // build output
+    $o = '';
+    $o .= '<video autoplay loop muted playsinline ';
+    if ($wporg_atts['controls']) $o .= 'controls ';
+    $o .= 'class="videogif"';
+    if (!is_null($wporg_atts['style'])) $o .= 'style="' . $wporg_atts['style'] . '" ';
+    $o .= '><source src="' . $wporg_atts['mp4'] . '" type="video/mp4" />';
+    $o .= '<p>Your browser does not support the video element.</p></video>';
+
+    // return output
+    return $o;
+}
+add_shortcode( 'videogif', 'videogif' );
