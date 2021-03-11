9566bb1a1415213c77cbdf1f7a0c6b85782af100
RogerLianQing <68877438+RogerLianQing@users.noreply.github.com>
2021-03-11 15:24:30 -0500

    Update site-info.php

--- twentyseventeen/template-parts/footer/site-info.php
+++ twentyseventeen/template-parts/footer/site-info.php
@@ -9,11 +9,3 @@
  */
 
 ?>
-<div class="site-info">
-	<?php
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-	}
-	?>
-	
-</div><!-- .site-info -->
