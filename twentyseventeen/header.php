68c0ad0e69860be59d0f5277876b6a575d64b379
RogerLianQing <68877438+RogerLianQing@users.noreply.github.com>
2021-03-12 13:11:49 -0500

    Update header.php

--- twentyseventeen/header.php
+++ twentyseventeen/header.php
@@ -1,59 +1,1 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since Twenty Seventeen 1.0
- * @version 1.0
- */
-
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js no-svg">
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="https://gmpg.org/xfn/11">
-
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-
-		<?php if ( has_nav_menu( 'top' ) ) : ?>
-			<div class="navigation-top">
-				<div class="wrap">
-					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
-				</div><!-- .wrap -->
-			</div><!-- .navigation-top -->
-		<?php endif; ?>
-
-	</header><!-- #masthead -->
-
-	<?php
-
-	/*
-	 * If a regular post or page, and not the front page, show the featured image.
-	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
-	 */
-	if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
-		echo '<div class="single-featured-image-header">';
-		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
-
-	<div class="site-content-contain">
-		<div id="content" class="site-content">
-//idk
+<?php/** * The header for our theme * * This is the template that displays all of the <head> section and everything up until <div id="content"> * * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials * * @package WordPress * @subpackage Twenty_Seventeen * @since Twenty Seventeen 1.0 * @version 1.0 */?><!DOCTYPE html><html <?php language_attributes(); ?> class="no-js no-svg"><head><meta charset="<?php bloginfo( 'charset' ); ?>"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="profile" href="https://gmpg.org/xfn/11"><?php wp_head(); ?>			<!-- Start of Woopra Code --><script>  (function(){    var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)  })("woopra");  woopra.config({      domain: 'patentk.com'  });  woopra.track();</script><!-- End of Woopra Code -->				</head><body <?php body_class(); ?>><?php wp_body_open(); ?><div id="page" class="site">	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>	<header id="masthead" class="site-header" role="banner">		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>		<?php if ( has_nav_menu( 'top' ) ) : ?>			<div class="navigation-top">				<div class="wrap">					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>				</div><!-- .wrap -->			</div><!-- .navigation-top -->		<?php endif; ?>	</header><!-- #masthead -->	<?php	/*	 * If a regular post or page, and not the front page, show the featured image.	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().	 */	if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :		echo '<div class="single-featured-image-header">';		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );		echo '</div><!-- .single-featured-image-header -->';	endif;	?>	<div class="site-content-contain">		<div id="content" class="site-content">
